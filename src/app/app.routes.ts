952d2b26aeb1fba9c80c1ab77e3e9352c7669cd0
Fernando Herrera <fernando.herrera85@gmail.com>
2024-12-05 09:20:49 -0500

    refactor app configuration and routing; improve HTML structure and formatting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //  /hola mundo => HolaMundoComponent
+  // /admin => AdminComponent
+  // / => HomeComponent
+  // /404 => PageNotFoundComponent
+];
