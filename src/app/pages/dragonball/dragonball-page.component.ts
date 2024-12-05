f4ea5150ee128961214f6f844013fe9cfb409af8
Fernando Herrera <fernando.herrera85@gmail.com>
2024-12-05 16:16:11 -0500

    update dragonball page component; enhance character listing with power threshold and default message

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -17,6 +17,7 @@ export class DragonballPageComponent {
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 4, name: 'Yamcha', power: 500 },
     { id: 3, name: 'Piccolo', power: 3000 },
   ]);
 
