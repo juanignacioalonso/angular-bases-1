9b671b2dfe7d7f58b68f4df5d735be9a58356723
Fernando Herrera <fernando.herrera85@gmail.com>
2024-12-06 14:47:36 -0500

    refactor character addition component; move to new directory and update event emission

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
-import { CharacterAddComponent } from '../../components/dragonball/character-app/character-add.component';
+import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
 
 interface Character {
   id: number;
@@ -22,21 +22,8 @@ export class DragonballSuperPageComponent {
     { id: 2, name: 'Vegeta', power: 8000 },
   ]);
 
-  addCharacter() {
-    if (!this.name() || !this.power() || this.power() <= 0) {
-      return;
-    }
-
-    const newCharacter: Character = {
-      // id: this.characters().length + 1,
-      id: 10000,
-      name: this.name(),
-      power: this.power(),
-    };
-
-    // this.characters.update((list) => [...list, newCharacter]);
-    console.log({ newCharacter });
-    this.resetFields();
+  addCharacter(character: Character) {
+    this.characters.update((list) => [...list, character]);
   }
 
   resetFields() {
