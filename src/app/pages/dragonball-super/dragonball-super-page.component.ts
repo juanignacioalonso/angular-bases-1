af5b5ec3c8993f8e4ea08d5735de42001f34499d
Fernando Herrera <fernando.herrera85@gmail.com>
2024-12-06 13:43:08 -0500

    add dragonball-super page component; implement character addition and listing functionality

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,5 +1,4 @@
-import { NgClass } from '@angular/common';
-import { Component, computed, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 interface Character {
   id: number;
@@ -8,27 +7,17 @@ interface Character {
 }
 
 @Component({
-  templateUrl: './dragonball-page.component.html',
-  imports: [
-    // NgClass
-  ],
+  templateUrl: './dragonball-super-page.component.html',
+  selector: 'dragonball-super',
 })
-export class DragonballPageComponent {
+export class DragonballSuperPageComponent {
   name = signal('');
   power = signal(0);
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-    // { id: 2, name: 'Vegeta', power: 8000 },
-    // { id: 4, name: 'Yamcha', power: 500 },
-    // { id: 3, name: 'Piccolo', power: 3000 },
+    { id: 2, name: 'Vegeta', power: 8000 },
   ]);
-
-  // powerClasses = computed(() => {
-  //   return {
-  //     'text-danger': true,
-  //   };
-  // });
 
   addCharacter() {
     if (!this.name() || !this.power() || this.power() <= 0) {
