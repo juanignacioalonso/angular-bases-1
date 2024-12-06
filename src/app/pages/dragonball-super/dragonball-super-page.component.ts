95ba21624b0038c539c0f9e25f7f042d4b3c5ab7
Fernando Herrera <fernando.herrera85@gmail.com>
2024-12-06 15:23:30 -0500

    refactor dragonball-super page; integrate dragonball service for character management

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,12 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
 import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
-
-interface Character {
-  id: number;
-  name: string;
-  power: number;
-}
+import { DragonballService } from '../../services/dragonball.service';
 
 @Component({
   templateUrl: './dragonball-super-page.component.html',
@@ -14,20 +9,5 @@ interface Character {
   imports: [CharacterListComponent, CharacterAddComponent],
 })
 export class DragonballSuperPageComponent {
-  name = signal('');
-  power = signal(0);
-
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
-
-  addCharacter(character: Character) {
-    this.characters.update((list) => [...list, character]);
-  }
-
-  resetFields() {
-    this.name.set('');
-    this.power.set(0);
-  }
+  public dragonballService = inject(DragonballService);
 }
