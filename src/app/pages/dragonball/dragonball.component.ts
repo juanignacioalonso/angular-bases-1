ad4060c824f8a831756e8f8404a1b211c2ca0546
juanignacioalonso <zalbak87@gmail.com>
2025-03-20 22:26:52 -0300

    Aparte 55

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -14,6 +14,7 @@ interface Character {
 })
 export class DragonballPageComponent {
 
+
   name = signal('Gohan');
   power = signal(100);
 
@@ -24,4 +25,23 @@ export class DragonballPageComponent {
     { id: 3, name: 'Yamcha', power: 500 },
   ]);
 
+  addCharacter() {
+    if(!this.name() || !this.power()|| this.power() <= 0){
+      return;
+    }
+
+    const newCaracter :Character ={
+      id: this.characters().length + 1,
+      name: this.name(),
+      power:this.power()
+    }
+
+    this.characters.update((list)=>[...list,newCaracter]);
+    this.resetFields();
+  }
+
+  resetFields(){
+    this.name.set('');
+    this.power.set(0);
+  }
 }
