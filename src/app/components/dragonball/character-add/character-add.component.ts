d8901a86fdbbb0cadd3a5a5835535367aaf34a47
juanignacioalonso <zalbak87@gmail.com>
2025-03-29 13:22:44 -0300

    Output-59

--- src/app/components/dragonball/character-add/character-add.component.ts
+++ src/app/components/dragonball/character-add/character-add.component.ts
@@ -1,4 +1,4 @@
-import {  Component, signal } from '@angular/core';
+import {  Component, output, signal } from '@angular/core';
 import { Character } from '../../../interfaces/character.interface';
 
 @Component({
@@ -9,6 +9,7 @@ export class CharacterAddComponent {
   name = signal('');
   power = signal(0);
 
+  newCharacter = output<Character>();
 
   addCharacter() {
     if(!this.name() || !this.power()|| this.power() <= 0){
@@ -16,14 +17,13 @@ export class CharacterAddComponent {
     }
 
     const newCaracter :Character ={
-      //id: this.characters().length + 1,
-      id:1000,
+      id:Math.floor(Math.random()*1000),
       name: this.name(),
       power:this.power()
     }
 
     //this.characters.update((list)=>[...list,newCaracter]);
-    console.log(newCaracter);
+    this.newCharacter.emit(newCaracter);
     this.resetFields();
   }
 
--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
-import { CharacterAddComponent } from "../../components/dragonball/character-app/character-add.component";
+import { CharacterAddComponent } from "../../components/dragonball/character-add/character-add.component";
 
 interface Character {
   id: number;
@@ -25,19 +25,8 @@ export class DragonballSuperPageComponent {
     { id: 2, name: 'Vegeta', power: 9000 },
   ]);
 
-  addCharacter() {
-    if(!this.name() || !this.power()|| this.power() <= 0){
-      return;
-    }
-
-    const newCaracter :Character ={
-      id: this.characters().length + 1,
-      name: this.name(),
-      power:this.power()
-    }
-
-    this.characters.update((list)=>[...list,newCaracter]);
-    this.resetFields();
+  addCharacter(character:Character) {
+    this.characters.update((list)=>[...list,character])
   }
 
   resetFields(){
